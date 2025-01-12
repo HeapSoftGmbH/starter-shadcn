61235856c97e8b40c0337c854ef26385a453e985
FrediLehmann <frederic.lehmann@bluewin.ch>
2025-01-12 13:50:45 +0100

    upgrade packages

--- src/lib/store/StoreProvider.tsx
+++ src/lib/store/StoreProvider.tsx
@@ -6,7 +6,8 @@ import { Provider } from 'react-redux';
 import { AppStore, makeStore } from '@/lib/store';
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
-	const storeRef = useRef<AppStore>();
+	const storeRef = useRef<AppStore>(null);
+
 	if (!storeRef.current) {
 		storeRef.current = makeStore();
 	}
